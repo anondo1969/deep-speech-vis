68aa134fa0bbe0aa7890ff31e7b48a1597aa912f
Mahbub Ul Alam <alammb@ims.uni-stuttgart.de>
2017-09-18 18:25:11 +0200

    Upload New File

---
+++ deep_speech_vis/visualization/__init__.py
@@ -0,0 +1,6 @@
+'''@package visualization
+contains the following functionality:
+- visualize a single uttterance after decoding
+- visualize in different relevance methods
+- scale the image according to the choice
+'''
