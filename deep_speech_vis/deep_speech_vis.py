22a465195067a9a2fde21d8e08f9adcf67540709
Mahbub Ul Alam <alammb@ims.uni-stuttgart.de>
2017-09-20 15:49:18 +0200

    Replace deep_speech_vis.py

--- deep_speech_vis/deep_speech_vis.py
+++ deep_speech_vis/deep_speech_vis.py
@@ -2,7 +2,7 @@
 run this file to go through the neural net training procedure, look at the 'deep_speech_vis_configuration.cfg' file to modify the settings'''
 import os
 from six.moves import configparser
-from feature_extraction import train_features_extraction, train_padded_batch_data_saving, test_features_extraction
+from feature_extraction import train_features_extraction, test_features_extraction
 from neural_network import simple_NN_training, simple_NN_decoding
 from kaldi_processes import ark
 from visualization import utterance_visualization, analyze_relevance
@@ -13,9 +13,8 @@ import pickle
 # any module succefully more than once
 
 # All modules
-TRAIN_FEATURE_EXTRACTION = False
-TRAIN_SAVE_PADDED_DATA = False
-TRAIN_NN = True
+TRAIN_FEATURE_EXTRACTION = True
+TRAIN_NN = False
 TEST_FEATURE_EXTRACTION = False
 DECODE_NN = False
 DECODE_KALDI = False
@@ -41,45 +40,27 @@ if TRAIN_FEATURE_EXTRACTION:
 
     train_features = train_features_extraction.features_extraction(context_width, ark_file_name, save_dir, batch_size, pdf_file_dir, pdf_file_total)
 
-    train_important_information = train_features.data_processing()
+    #before calling please check in the save directory the utterance dictionary is saved
+    #already or not and give the 'utterances_save' value accordingly.
+    #It will save a lot of time.
+    train_important_information = train_features.batch_data_processing(utterances_save=False)
 
     print train_important_information
 
     print "Train feature extraction is completed."
-
-if TRAIN_SAVE_PADDED_DATA:
-
-    if not train_important_information:
-        save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-        with open(save_dir+"/train_important_info", "rb") as fp:
-            train_important_information = pickle.load(fp)
-    
-    feat_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-    save_dir = config.get('directories', 'exp_dir') + '/train_padded_batch_dir'
-    total_batch_number = train_important_information['training_batch_total']
-    max_length = train_important_information['train_utt_max_length']
-
-    train_padded_batch_save = train_padded_batch_data_saving.Padded_Batch_Data_Save(feat_dir, save_dir, total_batch_number, max_length)
-
-    train_padded_batch_save.save_batch_data()
-
-    print "Train batch data are zero padded and saved"
 
 
 if TRAIN_NN:
     
     if not train_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-        with open(save_dir+"/train_important_info", "rb") as fp:
-            train_important_information = pickle.load(fp)
+        train_important_information = train_features_extraction.get_important_info(save_dir)
 
     n_classes = train_important_information['num_labels']
     input_dim = train_important_information['input_dim']
     total_batch = train_important_information['training_batch_total']
     max_input_length = train_important_information['train_utt_max_length']
-    #will change to 'train_important_information['train_label_max_length']' later
-    #after running train extract again
-    max_target_length = train_important_information['train_utt_max_length']
+    max_target_length = train_important_information['train_label_max_length']
 
     trainer = simple_NN_training.Simple_multy_layer_perceptron()
     trainer.train_NN(config, n_classes, input_dim, total_batch, max_input_length, max_target_length)
@@ -107,13 +88,11 @@ if DECODE_NN:
 
     if not train_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-        with open(save_dir+"/train_important_info", "rb") as fp:
-            train_important_information = pickle.load(fp)
+        train_important_information = train_features_extraction.get_important_info(save_dir)
 
     if not test_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-        with open(save_dir+"/test_important_info", "rb") as fp:
-            test_important_information = pickle.load(fp)
+        test_important_information = test_features_extraction.get_important_info(save_dir)
 
     max_length = test_important_information['test_utt_max_length']
     input_dim = train_important_information['input_dim']
@@ -137,6 +116,8 @@ if DECODE_KALDI:
 
     print '------- decoding testing sets using kaldi decoder ----------'
 
+    decode_dir = config.get('directories', 'exp_dir') + '/NN_decode_dir'
+
     #copy the gmm model and some files to speaker mapping to the decoding dir
     os.system('cp %s %s' %(config.get('directories', 'exp_dir') + '/' + config.get('general', 'gmm_name') + '/final.mdl', decode_dir))
     os.system('cp -r %s %s' %(config.get('directories', 'exp_dir') + '/' + config.get('general', 'gmm_name') + '/graph', decode_dir))
@@ -159,13 +140,11 @@ if VISUALIZE_UTTERANCE:
 
     if not train_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-        with open(save_dir+"/train_important_info", "rb") as fp:
-            train_important_information = pickle.load(fp)
+        train_important_information = train_features_extraction.get_important_info(save_dir)
 
     if not test_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-        with open(save_dir+"/test_important_info", "rb") as fp:
-            test_important_information = pickle.load(fp)
+        test_important_information = test_features_extraction.get_important_info(save_dir)
 
     random_utterance_id = int (config.get('visualization', 'random_utterance_id'))
     total_uttarences = test_important_information['total_test_utterances']
