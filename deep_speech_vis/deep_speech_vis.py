9f91609f496052ec3a9b5ed8f86a4fea2bce873c
Mahbub Ul Alam <alammb@ims.uni-stuttgart.de>
2017-09-20 17:39:46 +0200

    Replace deep_speech_vis.py

--- deep_speech_vis/deep_speech_vis.py
+++ deep_speech_vis/deep_speech_vis.py
@@ -30,15 +30,8 @@ train_important_information = {}
 test_important_information = {}
 
 if TRAIN_FEATURE_EXTRACTION:
-
-    context_width = int(config.get('simple_NN', 'context_width'))
-    ark_file_name = config.get('directories', 'train_ark')
-    save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-    batch_size = int(config.get('simple_NN', 'batch_size'))
-    pdf_file_dir = config.get('directories', 'exp_dir') + '/' + config.get('general', 'gmm_name')
-    pdf_file_total = int(config.get('general', 'num_pdf_files'))
-
-    train_features = train_features_extraction.features_extraction(context_width, ark_file_name, save_dir, batch_size, pdf_file_dir, pdf_file_total)
+   
+    train_features = train_features_extraction.features_extraction(config)
 
     #before calling please check in the save directory the utterance dictionary is saved
     #already or not and give the 'utterances_save' value accordingly.
@@ -56,26 +49,15 @@ if TRAIN_NN:
         save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
         train_important_information = train_features_extraction.get_important_info(save_dir)
 
-    n_classes = train_important_information['num_labels']
-    input_dim = train_important_information['input_dim']
-    total_batch = train_important_information['training_batch_total']
-    max_input_length = train_important_information['train_utt_max_length']
-    max_target_length = train_important_information['train_label_max_length']
-
     trainer = simple_NN_training.Simple_multy_layer_perceptron()
-    trainer.train_NN(config, n_classes, input_dim, total_batch, max_input_length, max_target_length)
+    trainer.train_NN(config, train_important_information)
 
     print "Neural network training is completed"
 
 
 if TEST_FEATURE_EXTRACTION:
 
-    context_width = int(config.get('simple_NN', 'context_width'))
-    ark_file_name = config.get('directories', 'test_ark')
-    save_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-    batch_size = int(config.get('simple_NN', 'batch_size'))
-
-    test_features = test_features_extraction.features_extraction(context_width, ark_file_name, save_dir, batch_size)
+    test_features = test_features_extraction.features_extraction(config)
 
     test_important_information = test_features.data_processing()
 
@@ -93,19 +75,14 @@ if DECODE_NN:
     if not test_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
         test_important_information = test_features_extraction.get_important_info(save_dir)
-
-    max_length = test_important_information['test_utt_max_length']
-    input_dim = train_important_information['input_dim']
-    total_uttarences = test_important_information['total_test_utterances']
-    load_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-    decode_dir = config.get('directories', 'exp_dir') + '/NN_decode_dir'
             
-    decoder = simple_NN_decoding.Decode(config, max_length, input_dim, total_uttarences, load_dir, decode_dir)
+    decoder = simple_NN_decoding.Decode(config, train_important_information, test_important_information)
 
+    decode_dir = config.get('directories', 'exp_dir') + '/NN_decode_dir'
     #create an ark writer for the likelihoods
     if os.path.isfile(decode_dir + '/likelihoods.ark'):
         os.remove(decode_dir + '/likelihoods.ark')
-
+    
     writer = ark.ArkWriter(decode_dir + '/feats.scp', decode_dir + '/likelihoods.ark')
 
     decoder.decode_data(writer)
@@ -140,38 +117,26 @@ if VISUALIZE_UTTERANCE:
 
     if not train_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/train_features_dir'
-        train_important_information = train_features_extraction.get_important_info(save_dir)
+        train_info = train_features_extraction.get_important_info(save_dir)
 
     if not test_important_information:
         save_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-        test_important_information = test_features_extraction.get_important_info(save_dir)
+        test_info = test_features_extraction.get_important_info(save_dir)
 
     random_utterance_id = int (config.get('visualization', 'random_utterance_id'))
     total_uttarences = test_important_information['total_test_utterances']
 
     if random_utterance_id < 0 or random_utterance_id >= total_uttarences:
 
-        print "Invalid random utterance ID. Please select a number between 0 & "+str(total_uttarences - 1)+'.'
+        print "Wrong random utterance ID. Please pick a number between 0 & "+str(total_uttarences - 1)+'.'
 
     else:
-
-        utt_dir = config.get('directories', 'exp_dir') + '/test_features_dir'
-        save_dir = config.get('directories', 'exp_dir') + '/NN_train_dir'
-        max_length = test_important_information['test_utt_max_length']
-        input_dim = train_important_information['input_dim']
-        image_dir = config.get('directories', 'exp_dir') + '/heat_map_image_dir'
-
-        relevance_method_name = config.get('visualization', 'visualization_method')
-        epsilon_value = float(config.get('visualization', 'epsilon_value'))
-        alpha_value = float(config.get('visualization', 'alpha_value'))
-        pixel_scaling_factor = int(config.get('visualization', 'pixel_scaling_factor'))
-        extension = config.get('visualization', 'extension')
 
         relevance_methods = ['simple_lrp', 'flat_lrp', 'ww_lrp', 'epsilon_lrp', 'alphabeta_lrp']
 
         if relevance_method_name in relevance_methods:
 
-            visualize = utterance_visualization.Visualize_single_utterance(random_utterance_id, utt_dir, save_dir, max_length, input_dim, image_dir)
+            visualize = utterance_visualization.Visualize_single_utterance(config, train_info, test_info)
 
             epochs = int(config.get('simple_NN', 'training_epochs'))
 
@@ -179,7 +144,7 @@ if VISUALIZE_UTTERANCE:
 
                 results, weights, biases = visualize.decode_data(epoch+1)
 
-                analyze = analyze_relevance.relevance_analyzer(results, weights, biases, epoch+1, relevance_method_name, epsilon_value, alpha_value, pixel_scaling_factor, extension, image_dir)
+                analyze = analyze_relevance.relevance_analyzer(results, weights, biases, epoch+1, config)
 
             print "Single utterance visualization is completed"
 
